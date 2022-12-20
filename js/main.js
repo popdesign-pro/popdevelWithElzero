6b6d03c47d6e1bec8bb79cd100512ce0aaeef286
popdevel <pop.design@icloud.com>
2022-12-20 21:25:27 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ console.log(
   "color:pink;font-weight:bold; font-size:20px;",
   "color:orange; font-size:20px;font-weight:bold;",
 );
+// Create Elements
+let myWay = document.querySelector(".js");
+myWay.className = "shadow-lg js mt-4 p-3 shadow container";
+let myHead = document.createElement("h2");
+myHead.className = "text-primary fs-20";
+let myHeadIn = document.createTextNode(`My only way`);
+let myText = document.createElement("p");
+let myTextIn = document.createTextNode("Hello in my world Guys");
+// reOrder Elements inside each other
+document.body.appendChild(myWay);
+myWay.appendChild(myHead);
+myHead.appendChild(myHeadIn);
+myWay.appendChild(myText);
+myText.appendChild(myTextIn);
